d9d89d8d3edad636ba63fc7ba88bbca42f065f49
theemadnes <alex.mattson@gmail.com>
2022-10-20 23:09:14 -0500

    basic requests working

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/common-nighthawk/go-figure"
 )
 
 // set default port number if env var $PORT isn't set
@@ -15,15 +18,27 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		fmt.Printf("Req: %s\n", r.URL.Path)
+		//http.NotFound(w, r)
+		str := strings.Replace(r.URL.Path, "/", " ", -1) // replace slashes in path with spaces
+		asciiStr := figure.NewFigure(str, "", true)
+		io.WriteString(w, asciiStr.String())
+		return
+	}
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+	str := "base path"
+	asciiStr := figure.NewFigure(str, "", true)
+	io.WriteString(w, asciiStr.String())
 }
 
 func main() {
 	port := getEnv("PORT", "8080")
 
-	http.HandleFunc("/", getRoot)
+	//http.HandleFunc("/*")
+	http.HandleFunc("/", getHandler)
 
 	http.ListenAndServe(":"+port, nil)
 }
