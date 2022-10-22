934a2604d36819f3917163a25457dc0e7d39754b
theemadnes <alex.mattson@gmail.com>
2022-10-22 00:15:49 -0500

    lowercasing font values

--- main.go
+++ main.go
@@ -26,15 +26,15 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		fmt.Printf("Req: %s\n", r.URL.Path)
 		//http.NotFound(w, r)
-		str := strings.Replace(r.URL.Path, "/", " ", -1)               // replace slashes in path with spaces so `hi/there` results in `hi there`
-		str = strings.TrimSpace(str)                                   // trim leading & trailing whitespace
-		asciiStr := figure.NewFigure(str, r.Header.Get("font"), false) // `false` means we'll replace non-ASCII chars with `?`
+		str := strings.Replace(r.URL.Path, "/", " ", -1)                                // replace slashes in path with spaces so `hi/there` results in `hi there`
+		str = strings.TrimSpace(str)                                                    // trim leading & trailing whitespace
+		asciiStr := figure.NewFigure(str, strings.ToLower(r.Header.Get("font")), false) // `false` means we'll replace non-ASCII chars with `?` // lowercasing font header values
 		io.WriteString(w, asciiStr.String())
 		return
 	}
 	fmt.Printf("got / request\n")
 	str := "base path"
-	asciiStr := figure.NewFigure(str, r.Header.Get("font"), true)
+	asciiStr := figure.NewFigure(str, strings.ToLower(r.Header.Get("font")), true) // lowercasing font header values
 	io.WriteString(w, asciiStr.String())
 }
 
